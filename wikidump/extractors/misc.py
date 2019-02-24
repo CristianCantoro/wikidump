213d610c3c5b098b0381007a937866b7feab8e5b
Cristian Consonni <cristian@balist.es>
2019-02-24 22:05:28 +0100

    Use complete regex directly

--- wikidump/extractors/misc.py
+++ wikidump/extractors/misc.py
@@ -270,15 +270,15 @@ wikilink_re = regex.compile(
      ''', regex.VERBOSE | regex.MULTILINE)
 
 
-wikilink_simple_re = regex.compile(
-    r'''\[\[                # Match two opening brackets
-         [^\n\]\[]+?
-        \]\]                # Match two closing brackets
-     ''', regex.VERBOSE | regex.MULTILINE)
+# wikilink_simple_re = regex.compile(
+#     r'''\[\[                # Match two opening brackets
+#          [^\n\]\[]+?
+#         \]\]                # Match two closing brackets
+#      ''', regex.VERBOSE | regex.MULTILINE)
 
 
-# match any non-alphanumeric character
-endanchor_re = regex.compile(r'\W')
+# # match any non-alphanumeric character
+# endanchor_re = regex.compile(r'\W')
 
 # the regex module supports reverse search
 # https://pypi.org/project/regex/
@@ -320,26 +320,12 @@ def wikilinks(page_title: str,
         -> Iterator[CaptureResult[Wikilink]]:
     """Return the wikilinks found in the document."""
 
-    wikilink_simple_matches = empty_generator()
+    wikilink_matches = empty_generator()
 
-    # We could try to find instances of parenthesys, but we try first to
-    # just find whole links with the simple regex. After all, many pages will
-    # have links and this check is not going to be very helpful.
-    # if source.find('[[') is not -1 \
-    #         and source.find(']]') is not -1:
-    #     # equivalent to
-    #     #  if '[[' in line and ']]' in line
-    #     # but way more efficient
-    #     # https://www.agnosticdev.com/content/
-    #     #   how-find-substring-string-python
-    #     wikilink_simple_matches = peekable(
-    #         wikilink_simple_re.finditer(source,concurrent=True))
-    # else:
-    #     return
     if debug:
         try:
-            wikilink_simple_matches = timeout.wrap_timeout(
-                lambda t: peekable(wikilink_simple_re.finditer(t,concurrent=True)),
+            wikilink_matches = timeout.wrap_timeout(
+                lambda t: peekable(wikilink_re.finditer(t,concurrent=True)),
                 REGEX_TIMEOUT,
                 [source]
                 )
@@ -347,8 +333,8 @@ def wikilinks(page_title: str,
             import ipdb; ipdb.set_trace()
 
     else:
-        wikilink_simple_matches = peekable(
-            wikilink_simple_re.finditer(source,concurrent=True)
+        wikilink_matches = peekable(
+            wikilink_re.finditer(source,concurrent=True)
             )
 
     sections_limits = [SectionLimits(name=section.name,
@@ -361,131 +347,80 @@ def wikilinks(page_title: str,
     last_section_seen = 0
     prevmatch_start = 0
     prevmatch_end = 0
-    for simple_match in wikilink_simple_matches:
+    for match in wikilink_matches:
+        prevmatch_start = match.start()
+        prevmatch_end = match.end()
 
-        # start at opening square brakcets '[['
-        prev_pos = simple_match.start()
+        link = match.group('link') or ''
+        link = link.strip()
 
-        post_match = endanchor_re.search(
-            source[simple_match.end():]
-            )
-        if post_match:
-            post_pos = simple_match.end() + post_match.start()
-        else:
-            # end of text
-            post_pos = simple_match.end()
-
-        match = None
-        # subtext = source[prev_pos:post_pos]
-        if debug:
-            try:
-                match = timeout.wrap_timeout(
-                    wikilink_re.search,
-                    REGEX_TIMEOUT,
-                    [source[prev_pos:post_pos]]
-                    )
-            except CallTimeout as exception:
-                import ipdb; ipdb.set_trace()
-        else:
-            match = wikilink_re.search(
-                source[prev_pos:post_pos]
-                )
-
-        # more memorable name
-        wikilink_re_start = prev_pos
-        if match is None:
-            simple_match_text = (simple_match.group(0)
-                                 .strip()
-                                 .lstrip('[')
-                                 .rstrip(']')
-                                 )
-
-            # wikilink_re is not matching because of illegal characters in
-            # the link:
-            if simple_match_text.find('|') or \
-                    simple_match_text.find('>') or \
-                    simple_match_text.find('<') or \
-                    simple_match_text.find('{') or \
-                    simple_match_text.find('}'):
-                continue
-            else:
-                # this is unexpected
-                raise ValueError('Unexpected empty match')
-
-        else:
-            prevmatch_start = wikilink_re_start + match.start()
-            prevmatch_end = wikilink_re_start + match.end()
-
-            link = match.group('link') or ''
-            link = link.strip()
-
-            # split on '#' (link to section)
-            tosection = ''
-            if '#' in link:
-                splitlink = link.split('#', 1)
-                link = splitlink[0]
-                if not link:
-                    link = page_title
-                tosection = splitlink[1]
-
-            anchor = match.group('anchor') or link
-
-            # newlines in anchor are visualized as spaces.
-            anchor = anchor.replace('\n', ' ')
-            anchor = ' '.join(anchor.strip().split())
-
-            total_start = wikilink_re_start + match.start('total')
-            total_end = wikilink_re_start + match.end('total')
-
-            link_section_number = 0
-            link_section_name = '---~--- incipit ---~---'
-            link_section_level = 0
-
-            for section in sections_limits[last_section_seen:]:
-                if section.begin <= total_start <= section.end:
-                    link_section_number = section.number
-                    link_section_name = section.name
-                    link_section_level = section.level
-                    last_section_seen = (link_section_number - 1)\
-                        if link_section_number > 0 else 0
-                    break
-
-            # There are cases in which in the wikitext you will find cases
-            # such as:
-            #   - [[ |yoda]]
-            #   - [[|diety|god]]
-            #   - [[]]
-            # Note: * the first case never occours, because the (software)
-            #       editor would auto-correct it in [[yoda]], it may be
-            #       something that could occour in a past version of the
-            #       software.
-            #       * in the last case both 'link' and 'anchor' are empty)
-            #
-            # We consider these cases to be broken no matter what and we
-            # ignore them
+        # split on '#' (link to section)
+        tosection = ''
+        if '#' in link:
+            splitlink = link.split('#', 1)
+            link = splitlink[0]
             if not link:
-                continue
+                link = page_title
+            tosection = splitlink[1]
 
-            wikilink = Wikilink(
-                link=link,
-                anchor=anchor,
-                tosection=tosection,
-                section_name=link_section_name,
-                section_level=link_section_level,
-                section_number=link_section_number
-            )
+        anchor = match.group('anchor') or link
 
-            anchor_prefix = (source[wikilink_re_start+match.start('total'):
-                                    wikilink_re_start+match.start('wikilink')]
-                             .strip('[')
-                             )
-            anchor_suffix = (source[wikilink_re_start+match.end('wikilink'):
-                                    wikilink_re_start+match.end('total')]
-                             .strip(']')
-                             )
-            anchor = anchor_prefix + anchor + anchor_suffix
+        # newlines in anchor are visualized as spaces.
+        anchor = anchor.replace('\n', ' ')
+        anchor = ' '.join(anchor.strip().split())
 
-            # print(source[total_start:total_end])
-            yield CaptureResult(wikilink, Span(total_start, total_end))
+        total_start = match.start('total')
+        total_end = match.end('total')
+
+        link_section_number = 0
+        link_section_name = '---~--- incipit ---~---'
+        link_section_level = 0
+
+        for section in sections_limits[last_section_seen:]:
+            if section.begin <= total_start <= section.end:
+                link_section_number = section.number
+                link_section_name = section.name
+                link_section_level = section.level
+                last_section_seen = (link_section_number - 1)\
+                    if link_section_number > 0 else 0
+                break
+
+        # There are cases in which in the wikitext you will find cases
+        # such as:
+        #   - [[ |yoda]]
+        #   - [[|diety|god]]
+        #   - [[]]
+        # Note: * the first case never occours, because the (software)
+        #       editor would auto-correct it in [[yoda]], it may be
+        #       something that could occour in a past version of the
+        #       software.
+        #       * in the last case both 'link' and 'anchor' are empty)
+        #
+        # We consider these cases to be broken no matter what and we
+        # ignore them
+        if not link:
+            continue
+
+        wikilink = Wikilink(
+            link=link,
+            anchor=anchor,
+            tosection=tosection,
+            section_name=link_section_name,
+            section_level=link_section_level,
+            section_number=link_section_number
+        )
+
+        anchor_prefix = (source[match.start('total'):
+                                match.start('wikilink')]
+                         .strip('[')
+                         )
+        anchor_suffix = (source[match.end('wikilink'):
+                                match.end('total')]
+                         .strip(']')
+                         )
+        anchor = anchor_prefix + anchor + anchor_suffix
+
+        # print(source[total_start:total_end])
+        yield CaptureResult(wikilink, Span(total_start, total_end))
 
     return
