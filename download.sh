f2993e165a6a13475b77147c92f89de1edc08682
Cristian Consonni <cristian@balist.es>
2025-03-15 10:16:08 +0100

    Rewrite script

--- download.sh
+++ download.sh
@@ -1,113 +1,200 @@
 #!/usr/bin/env bash
+# shellcheck disable=SC2128
+SOURCED=false && [ "$0" = "$BASH_SOURCE" ] || SOURCED=true
 
+#################### CLI
 help="
-Usage: download.sh [-f FILTER] [-l LIST] [-n] [-o OUTDIR] [-v] -d DATE PROJECT
-       download.sh [-v] --md5-only -d DATE PROJECT
-       download.sh (-h | --help)
+Usage: download.sh [-d] [-v] [-o OUTPUT_BASE] [-a ALGORITHM]
+                   [-e FILTER_REGEX] [-f FILTER_FIXED]
+                   [-s FILTER_STRING]
+                   PROJECT DATE
+       download.sh (-h | -V)
 
 Options:
-      -d, --date DATE           Dump date (format: yyyymmdd).
-      -f, --filter FILTER       Filter files names to download (defult: 'history').
-      -l, --list LIST           List of files to download (default: download MD5 file).
-                                (implies --md5-no-download)
-      --md5-only                Only download the MD5 sums file.
-      -n, --md5-no-download     Do not download the MD5 sums file.
-      -o, --output-dir OUTDIR   Output directory for dump files.
+      -d, --debug               Generate debug output (implies --verbose).
       -v, --verbose             Generate verbose output.
+      -o, --output-base OUTPUT_BASE
+                                Base output directory, downloaded will be
+                                downloaded to OUTPUT_BASE/PROJECT/DATE/
+                                [default: dumps].
+      -e, --filter-regex FILTER_REGEX
+                                Filter files names using a regular expression.
+      -f, --filter-fixed FILTER_FIXED
+                                List of files to download.
+      -s, --filter-string FILTER_STRING
+                                Filter files names searching for a string.
+      -a, --algorithm ALGORITHM
+                                Checksum algorithm, choose {md5, sha1}
+                                [default: md5].
       -h, --help                Show this help message and exits.
-      --version                 Print version and copyright information.
+      -V, --version             Print version and copyright information.
 ----
 download.sh is part of wikidump.
-download.sh needs docopts for CLI argument parsing.
+download.sh needs docopts (v. 0.6.4) for CLI argument parsing.
 "
-version='0.2'
+version='version 0.3'
 
-eval "$(docopts -A args -h "$help" : "$@")"
+eval "$(docopts -A args -V "$version" -h "$help" : "$@")"
+####################
 
-echo "The keys are: " ${!args[@]}
-echo "The values are: " ${args[@]}
-echo " "
-echo "Key <-> Value"
-echo "-------------"
-for i in "${!args[@]}"; do echo $i "<->" ${args[$i]}; done
-echo " "
+#################### Helpers
+declare -A filter_types
 
-# CLI arguments
-PROJECT="${args['PROJECT']}"
+filter_types["-s"]="string"
+filter_types["-e"]="regex"
+filter_types["-f"]="fixed"
 
-DATE="${args['--date']}"
-if [ -z "$DATE" ]; then
-    DATE='last'
+function get_last_filter {
+    local args_clone=("${@:2}")
+    filter_option=""
+    filter=""
+    for i in ${!args_clone[@]}; do
+        opt="${args_clone[$i]}"
+        opt_arg="${args_clone[$((i+1))]}"
+        case "$opt" in
+            -e|--filter-regex|-f|--filter-fixed|-s|--filter-string)
+                filter_option="$opt"
+                filter_type="${filter_types[$filter_option]}"
+                filter="$opt_arg"
+                shift 1
+                ;;
+            *)
+                shift
+                ;;
+        esac
+    done
+}
+
+function check_date_format {
+    local input="$1"
+
+    # Check if input is 'latest'
+    if [[ "$input" == 'latest' ]]; then
+        return 0  # Valid
+    fi
+
+    # Check if input matches the YYYYMMDD pattern
+    if [[ "$input" =~ ^[0-9]{8}$ ]]; then
+        # Validate if it's a real date (YYYYMMDD)
+        if date -d "${input:0:4}-${input:4:2}-${input:6:2}" >/dev/null 2>&1; then
+            return 0  # Valid
+        else
+            return 1  # Invalid date
+        fi
+    fi
+
+    return 1  # Invalid format
+}
+####################
+
+#################### Utils
+if ${args['--debug']}; then
+  # --debug implies --verbose
+  verbose=true
+  function echodebug {
+    (echo "$@" | ts '[%F %H:%M:%S][debug]:' 1>&2)
+  }
+else
+  function echodebug { true; }
 fi
-
-output_dir="${args['--output-dir']}"
-OUTDIR="$output_dir/$PROJECT/$DATE"
-if [ -z "$output_dir" ]; then
-    OUTDIR="dumps/$PROJECT/$DATE"
-fi
-
-LIST="${args['--list']}"
-md5_no_download=false
-if [ -z "$LIST" ]; then
-    md5_no_download=true
-fi
-
-FILTER="${args['--filter']}"
-if [ -z "$FILTER" ]; then
-    FILTER="history"
-fi
-
 
 if ${args['--verbose']}; then
-    echo "--- Download info ---"
-    echo "  * PROJECT: $PROJECT"
-    echo "  * DATE: $DATE"
-    echo "  * OUTDIR: $OUTDIR"
-    echo "  * LIST: $LIST"
-    echo "  * FILTER: $FILTER"
-    echo "  "
-    echo "  - md5_no_download: $md5_no_download"
-    echo "---"
-fi
-
-# global base URL
-BASE_URL="https://dumps.wikimedia.org/$PROJECT/$DATE/"
-
-# download the md5sum
-# https://dumps.wikimedia.org/itwiki/20151002/itwiki-20151002-md5sums.txt
-if $md5_no_download; then
-    if $verbose; then
-        echo ""
-        echo "Skipping MD5 download"
-        echo ""
-    fi
+  function echoverbose {
+    (echo "$@" | ts '[%F %H:%M:%S][info]:' 1>&2)
+  }
 else
-    MD5FILE="$PROJECT-$DATE-md5sums.txt"
-
-    if $verbose; then
-        echo ""
-        echo "Dowloading MD5 sums file $BASE_URL/$MD5FILE ..."
-        echo ""
-    fi
-
-    aria2c "$BASE_URL/$MD5FILE"
-
-    if [ ! -z "$md5_only" ]; then
-        echo ""
-        echo "---"
-        echo "Dowloading MD5 sums done!"
-        exit 0
-    fi
-    LIST="$MD5FILE"
+  function echoverbose { true; }
 fi
 
-# create OUTDIR and all the necessary intermediate directories
-mkdir -p "$OUTDIR"
+####################
+
+echodebug "Program start"
+echodebug "CLI args"
+echodebug "--------"
+for i in "${!args[@]}"; do
+    echodebug "  - $i:" ${args[$i]};
+done
+echodebug ""
+
+# CLI arguments
+project="${args['PROJECT']}"
+ddate="${args['DATE']}"
+
+if ! check_date_format "$ddate"; then
+    (echo "Error. Invalid date format, can be YYYYMMDD or 'latest'" 1>&2)
+    exit 1
+fi
+
+tmpdir=$(mktemp -d -t download_sh.XXXXXXXXXX)
+function cleanup {
+  rm -rf "$tmpdir"
+}
+trap cleanup EXIT
 
 
-set -x
-cut -f 3 -d ' ' "$LIST" | grep "$FILTER" | \
-  awk -v prefix=$BASE_URL '{print prefix $0}' | \
-  xargs -n 1 aria2c -x 3 -s 3 -c --force-sequential --dir="$OUTDIR"
+# CLI options
+output_base="${args['--output-base']}"
+output_dir="$output_base/$project/$ddate"
+
+get_last_filter "$@"
+echodebug "filter_type: $filter_type"
+echodebug "filter: $filter"
+
+echoverbose
+echoverbose "--- Download info ---"
+echoverbose "  * Project: $project"
+echoverbose "  * Date: $ddate"
+echoverbose "  * Output directory: $output_dir"
+echoverbose "  * Filter type: $filter_type"
+echoverbose "  * Filter: $filter"
+echoverbose
+
+# dump base URL
+BASE_URL="https://dumps.wikimedia.org/$project/$ddate"
+
+# download the checksums file
+#
+# checksum file: <project>-<date>-{md5,sha1}sums.txt
+# Example
+#   - https://dumps.wikimedia.org/itwiki/20151002/
+#       itwiki-20151002-md5sums.txt
+algorithm="${args['--algorithm']}"
+checksum_file="${project}-${ddate}-${algorithm}sums.txt"
+echodebug "checksum_file: $checksum_file"
+
+checksum_url="${BASE_URL}/${checksum_file}"
+echoverbose
+echoverbose "Dowloading MD5 sums file $checksum_url ..."
+echoverbose
+
+aria2c "$checksum_url"
+
+# download the dump files
+
+# create output_dir and all the necessary intermediate directories
+mkdir -p "$output_dir"
+
+selected_list=${tmpdir}/selected.txt
+if [[ "$filter_type" == 'regex' ]]; then
+    grep -E "$filter" "$checksum_file" > "$selected_list"
+elif [[ "$filter_type" == 'fixed' ]]; then
+    grep -F "$filter" "$checksum_file" > "$selected_list"
+elif [[ "$filter_type" == 'string' ]]; then
+    grep "$filter" "$checksum_file" > "$selected_list"
+else
+    (echo "Error. Invalid filter type, can be 'regex', 'fixed' or "
+          "'string'" 1>&2)
+    exit 1
+fi
+
+# prepare download list
+download_list=${tmpdir}/download.txt
+cut -f 3 -d ' ' "$selected_list" | \
+    awk -v base=$BASE_URL '{print base"/"$0}' > "$download_list"
+
+aria2c --max-concurrent-downloads 1 \
+       --deferred-input \
+       --dir="$output_dir" \
+       --input-file "$download_list"
 
 exit 0
